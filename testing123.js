ad9daec63b1547f53cd88699d6df17d61d291dea
John Moorman <45500240+mojoro@users.noreply.github.com>
2024-11-18 13:18:55 +0100

    Finish assignment

--- testing123.js
+++ testing123.js
@@ -14,12 +14,74 @@ function reverse(string) {
 
 export { reverse };
 
-const Calculator = () => {
-  const add = (a, b) => {
+const Calculator = {
+  add: (a, b) => {
     return a + b;
-  };
-
-  return { add };
+  },
+  subtract: (a, b) => {
+    return a - b;
+  },
+  divide: (a, b) => {
+    return a / b;
+  },
+  multiply: (a, b) => {
+    return a * b;
+  },
 };
 
 export { Calculator };
+
+// A-Z is 65-90, a-z is 97-122
+function caesarCipher(string, degree) {
+  let shiftedString = "";
+  for (let i = 0; i < string.length; i++) {
+    shiftedString += String.fromCharCode(
+      processCharCode(string.charCodeAt(i), degree)
+    );
+  }
+
+  return shiftedString;
+}
+
+// function to ignore other unicode values
+function isAlphabetical(charCode) {
+  if ((65 <= charCode && charCode <= 90) || (97 <= charCode && charCode <= 122))
+    return true;
+  else return false;
+}
+
+// function to return the opposite side of the corresponding range if the initial character is alphabetical
+function processCharCode(charCode, degree) {
+  if (degree > 26) degree = degree % 26;
+
+  if (!isAlphabetical(charCode) || degree == 0) return charCode;
+
+  let shiftedCode = charCode + degree;
+  if (isAlphabetical(shiftedCode)) return shiftedCode;
+
+  if (degree > 0) return shiftedCode - 26;
+  else if (degree < 0) return shiftedCode + 26;
+}
+
+export { caesarCipher };
+
+function analyzeArray(array) {
+  if (!Array.isArray(array)) throw new Error("Please pass in an array");
+  const isNotInteger = (element) => !Number.isInteger(element);
+  if (array.some(isNotInteger)) throw new Error("Must be an array of numbers!");
+  let average = 0,
+    sum = 0,
+    min = 0,
+    max = 0,
+    length = array.length;
+
+  array.sort((a, b) => a - b);
+  min = array[0];
+  max = array[length - 1];
+  sum = array.reduce((accumulator, current) => accumulator + current);
+  average = sum / length;
+
+  return { average, min, max, length };
+}
+
+export { analyzeArray };
--- testing123.test.js
+++ testing123.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverse, Calculator } from "./testing123";
+import {
+  capitalize,
+  reverse,
+  Calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./testing123";
 
 test("Capitalizes string", () => {
   expect(capitalize("greg")).toBe("Greg");
@@ -12,4 +18,58 @@ test("Reverses string", () => {
 
 test("Calculator adds", () => {
   expect(Calculator.add(2, 3)).toBe(5);
+});
+test("Calculator subtracts", () => {
+  expect(Calculator.subtract(2, 3)).toBe(-1);
+});
+test("Calculator divides", () => {
+  expect(Calculator.divide(3, 3)).toBe(1);
+});
+test("Calculator multiplies", () => {
+  expect(Calculator.multiply(2, 3)).toBe(6);
+});
+
+test("Cipher shifts", () => {
+  expect(caesarCipher("abc", 3)).toBe("def");
+});
+
+test("Cipher shifts caps", () => {
+  expect(caesarCipher("Abc", 3)).toBe("Def");
+});
+
+test("Cipher wraps", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+
+test("Cipher preserves punctuation", () => {
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+
+test("Large shift does not break capital shifting", () => {
+  expect(caesarCipher("Abc!", 52)).toBe("Abc!");
+});
+
+const arrayToAnalyze = [1, 8, 3, 4, 2, 6];
+test("Analyzes array and calculates average", () => {
+  expect(analyzeArray(arrayToAnalyze).average).toBe(4);
+});
+
+test("Analyzes array and calculates min", () => {
+  expect(analyzeArray(arrayToAnalyze).min).toBe(1);
+});
+
+test("Analyzes array and calculates max", () => {
+  expect(analyzeArray(arrayToAnalyze).max).toBe(8);
+});
+
+test("Analyzes array and calculates length", () => {
+  expect(analyzeArray(arrayToAnalyze).length).toBe(6);
+});
+
+test("Error if not array", () => {
+  expect(() => analyzeArray({})).toThrow();
+});
+
+test("Error if element is not integer", () => {
+  expect(() => analyzeArray([1, 2, 3, "hey"])).toThrow();
 });
